0a54594822c7dae7a4abf1cbf388d60c4e6468a5
Victor Kasap <kasap.victor@gmail.com>
2021-12-10 06:56:01 +0300

    feat: added test for JSON from __textures__

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 import gendiff from '../src/index.js';
 
 // /Users/victorkasap/Projects/frontend-project-lvl2/__tests__/gendiff.test.js
@@ -13,19 +13,32 @@ const __dirname = dirname(__filename);
 // /Users/victorkasap/Projects/frontend-project-lvl2/__fixtures__/file1.json
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const getContent = (path) => JSON.parse(fs.readFileSync((path), 'utf-8'));
+/*
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+ */
+const getContent = (file) => {
+	const ext = path.extname(file);
+	
+	switch (true) {
+		case ext === '.json':
+			return JSON.parse(fs.readFileSync((file), 'utf-8'));
+		
+		case ext === '.txt':
+			return fs.readFileSync((file), 'utf-8').trim();
+	}
+};
 
 test('Test flat JSON files', () => {
-	
-	
 	const json1 = getFixturePath('file1.json');
 	const json2 = getFixturePath('file2.json');
 	const diff = gendiff(json1, json2);
 	const expectedOfJson = getFixturePath('expected_json.txt');
-	const expectContent = fs.readFileSync((expectedOfJson), 'utf-8');
+	const expectContent = getContent(expectedOfJson);
 	
-	// console.log(diff);
-	//  {"- follow":"false","  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":"50","+ timeout":"20","+ verbose":"true"}
-	console.log(expectContent);
-	// expect(gendiff(json1, json2)).toEqual(expectedJson);
-})
+	expect(gendiff(json1, json2)).toEqual(expectContent);
+});
