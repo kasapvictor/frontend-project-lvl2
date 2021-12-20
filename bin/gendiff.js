978ca5eb41ee2400ba7c5d439bc4bc7c521be296
Victor Kasap <kasap.victor@gmail.com>
2021-12-20 08:28:43 +0300

    feat: added default option - stylish to commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ program
   .arguments('<file1> <file2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((file1, file2) => {
-    const format = program.opts().format;
+    const { format } = program.opts();
     console.log(genDiff(file1, file2, format));
   })
   .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import parse from './utils/parsers.js';
 import tree from './process/tree.js';
 import stylish from './formatter/stylish.js';
 
-export default (path1, path2, format) => {
+export default (path1, path2, format = 'stylish') => {
   const file1 = parse(path1);
   const file2 = parse(path2);
   const unforemattedData = tree(file1, file2);
-  
+
   switch (format) {
     case 'stylish':
       return stylish(unforemattedData);
