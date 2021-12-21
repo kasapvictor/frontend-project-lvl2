99d65890703495adebada9b8019a0a572e396f5b
Victor Kasap <kasap.victor@gmail.com>
2021-12-21 07:25:26 +0300

    feat: added expected files format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -20,7 +20,7 @@ test('2 JSON files', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
   const diff = gendiff(file1, file2);
-  const expectedFile = getFixturePath('expected.txt');
+  const expectedFile = getFixturePath('expectedStylis.txt');
   const expectContent = getContent(expectedFile);
 
   expect(diff).toEqual(expectContent);
@@ -30,7 +30,7 @@ test('2 YAML files', () => {
   const file1 = getFixturePath('file1.yaml');
   const file2 = getFixturePath('file2.yml');
   const diff = gendiff(file1, file2);
-  const expectedFile = getFixturePath('expected.txt');
+  const expectedFile = getFixturePath('expectedStylis.txt');
   const expectContent = getContent(expectedFile);
 
   expect(diff).toEqual(expectContent);
