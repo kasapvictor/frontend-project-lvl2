fa03c9a8f4781439a96032050df9e9bc93d19324
Victor Kasap <kasap.victor@gmail.com>
2021-12-25 21:25:14 +0300

    fix: fixed by Dinislam 25 - rename content to getData

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import parse from './process/parse.js';
 import tree from './process/tree.js';
 import formatData from './formatter/index.js';
 
-const content = (pathData) => {
+const getData = (pathData) => {
   const format = path.extname(pathData).replace('.', '');
   const data = fs.readFileSync(pathData, 'utf-8');
 
@@ -12,8 +12,8 @@ const content = (pathData) => {
 };
 
 export default (path1, path2, format = 'stylish') => {
-  const data1 = content(path1);
-  const data2 = content(path2);
+  const data1 = getData(path1);
+  const data2 = getData(path2);
   const unforemattedData = tree(data1, data2);
   const formattedData = formatData(format, unforemattedData);
 
